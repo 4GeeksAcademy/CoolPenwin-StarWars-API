b6612f5043a8403e84d615a297b59162bdaf7f45
Alberto <coolPenwin@proton.me>
2024-08-31 15:07:11 +0000

    estilos estilosos

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BB8Switch } from "./BB8switch";
+import Pattern from "./patronMovil.js"; // Asegúrate de importar tu componente Pattern
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    
+      <nav className="navbar navbar-light bg-light mb-3 patroned">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+        </Link>
+        <div className="">
+          <BB8Switch />
+        </div>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-primary">
+              Check the Context in action
+            </button>
+          </Link>
+        </div>
+      </nav>
+    
+  );
 };
--- src/js/views/single.js
+++ src/js/views/single.js
@@ -2,12 +2,23 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import "../../styles/single.css";
 
 export const Single = props => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
 	return (
 		<div className="jumbotron">
+
+
+<div class="star-wars-card">
+  <div class="card-content">
+    <div class="card-image"></div>
+    <span class="card-title">Luke Skywalker</span>
+    <button class="card-button">Ver más</button>
+  </div>
+</div>
+
 			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
 
 			<hr className="my-4" />
