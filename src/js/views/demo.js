267a89cbd0dd56b2294a51f21da4e365a8593677
Alberto <coolPenwin@proton.me>
2024-09-01 15:09:04 +0000

    trae datoos

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -5,58 +5,114 @@ import { LikeSwitch } from "../component/LikeSwitch";
 import "../../styles/demo.css";
 import violentKitty from "../../img/violentKitty.jpg";
 
-
-
 export const Demo = () => {
   const { store, actions } = useContext(Context);
 
+  useEffect(() => {
+    actions.loadCharacters();
+    actions.loadPlanets();
+    actions.loadStarships();
+  }, []);
+
   return (
     <div className="container">
-      <ul className="list-group">
-        {store.demo.map((item, index) => {
-          return (
-            <>
-              <div class="star-wars-card">
-                <div class="card-content">
-
-				<div class="image-placeholder"><img src={violentKitty} style={{ width: "100%", height: "100%", objectFit: "cover" }} /></div>
-                  <span class="card-title">Luke Skywalker <LikeSwitch/></span>
-                  <button class="card-button">Ver más</button>
-                </div>
+      <h1 className="text-danger my-3">Characters</h1>
+      <div
+        className="row flex-nowrap scrollbar"
+        id="style-15"
+        style={{ overflowX: "auto", maxWidth: "100vw" }}
+      >
+        {store.characters.map((character, index) => (
+          <div class="star-wars-card" key={index}>
+            <div class="card-content">
+              <div class="image-placeholder">
+                <img
+                  src={violentKitty}
+                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                />
               </div>
+              <span class="card-title">
+                {character.properties.name}
+                {/* gender={character.properties.gender}
+                  hair_color={character.properties.hair_color}
+                  eye_color={character.properties.eye_color}
+                  uid={character.uid} */}
+              </span>
+              <div className="row spacebetwen">
+                <button class="card-button">Ver más</button>
+                <LikeSwitch />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
 
-              <li
-                key={index}
-                className="list-group-item d-flex justify-content-between"
-                style={{ background: item.background }}
-              >
-                <Link to={"/single/" + index}>
-                  <span>Link to: {item.title}</span>
-                </Link>
-                {
-                  // Conditional render example
-                  // Check to see if the background is orange, if so, display the message
-                  item.background === "orange" ? (
-                    <p style={{ color: item.initial }}>
-                      Check store/flux.js scroll to the actions to see the code
-                    </p>
-                  ) : null
-                }
-                <button
-                  className="btn btn-success"
-                  onClick={() => actions.changeColor(index, "orange")}
-                >
-                  Change Color
-                </button>
-              </li>
-            </>
-          );
-        })}
-      </ul>
+      <h1 className="text-danger my-3">Planets</h1>
+      <div
+        className="row flex-nowrap scrollbar"
+        id="style-15"
+        style={{ overflowX: "auto", maxWidth: "100vw" }}
+      >
+        {store.planets.map((planet, index) => (
+          <div class="star-wars-card" key={index}>
+            <div class="card-content">
+              <div class="image-placeholder">
+                <img
+                  src={violentKitty}
+                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                />
+              </div>
+              <span class="card-title">
+              {planet.properties.name}
+                            {/* population={planet.properties.population}
+                            terrain={planet.properties.terrain}
+                            uid={planet.uid} */}
+              </span>
+              <div className="row spacebetwen">
+                <button class="card-button">Ver más</button>
+                <LikeSwitch />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+
+      <h1 className="text-danger my-3">Starships</h1>
+      <div
+        className="row flex-nowrap scrollbar"
+        id="style-15"
+        style={{ overflowX: "auto", maxWidth: "100vw" }}
+      >
+        {store.starships.map((starship, index) => (
+          <div class="star-wars-card" key={index}>
+            <div class="card-content">
+              <div class="image-placeholder">
+                <img
+                  src={violentKitty}
+                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                />
+              </div>
+              <span class="card-title">
+              {starship.properties.name}
+                            {/* cost_in_credits={starship.properties.cost_in_credits}
+                            passengers={starship.properties.passengers}
+                            uid={starship.uid} */}
+              </span>
+              <div className="row spacebetwen">
+                <button class="card-button">Ver más</button>
+                <LikeSwitch />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
       <br />
-      <Link to="/">
-        <button className="btn btn-primary">Back home</button>
-      </Link>
+      <br />
+      <br />
+      <br />
+    
     </div>
   );
 };
